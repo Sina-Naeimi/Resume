0a7e9b02d0a79649ddbb94024ea4ddd14d509c0a
sina naeimi <sina.naeimi.suarez@gmail.com>
2021-11-29 19:55:50 +0330

    first comit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SinaNaeimiResume.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
